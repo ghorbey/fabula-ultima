00b04f02c0dbb64a6f90739149f33e280233440e
GhorghorBey <s.paquay@gmail.com>
2023-11-26 10:05:38 +0100

    Create dist directory and make compiling works.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Fabula Ultima (Unofficial)
 Fabula Ultima (unofficial) System for FoundryVTT
+
+# How to make it run?
+
+* Ensure a simlink exist and point to the dist directory.
+* execute npm i
+* execute npm run build
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 const gulp = require('gulp');
 const prefix = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 const sass = require('gulp-sass')(require('sass'));
+const webp = require('gulp-webp');
 
 /* ----------------------------------------- */
-/*  Compile Sass
+/* Convert images
+/* ----------------------------------------- */
+const SYSTEM_IMAGES = [
+  'src/assets/**/*.{png,jpeg,jpg}',
+  'src/tokens/**/*.{png,jpeg,jpg}'
+];
+function compileImages() {
+  return gulp.src(SYSTEM_IMAGES, {base: 'src'})
+    .pipe(webp())
+    .pipe(gulp.dest('./dist'));
+};
+const imageTask = gulp.series(compileImages);
+
+/* ----------------------------------------- */
+/* Compile Sass
 /* ----------------------------------------- */
 
 // Small error handler helper function.
@@ -12,12 +27,11 @@ function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
-
-const SYSTEM_SCSS = ["scss/**/*.scss"];
+const SYSTEM_SCSS = ["src/scss/**/*.scss"];
 function compileScss() {
   // Configure options for sass output. For example, 'expanded' or 'nested'
   let options = {
-    outputStyle: 'expanded'
+    outputStyle: 'compressed'
   };
   return gulp.src(SYSTEM_SCSS)
     .pipe(
@@ -27,27 +41,99 @@ function compileScss() {
     .pipe(prefix({
       cascade: false
     }))
-    .pipe(gulp.dest("./css"))
+    .pipe(gulp.dest("./dist/styles"))
 }
-const css = gulp.series(compileScss);
+const cssTask = gulp.series(compileScss);
 
 /* ----------------------------------------- */
-/*  Watch Updates
+/* Delete files
+/* ----------------------------------------- */
+const SYSTEM_DELETE = ['dist'];
+function deleteFiles() {
+  return del('dist/**', {force: true});
+}
+const deleteTask = gulp.series(deleteFiles);
+
+/* ----------------------------------------- */
+/* Copy files
+/* ----------------------------------------- */
+
+const SYSTEM_JSON_FILES = [
+  'system.json',
+  'template.json'
+];
+function copyJsonFiles() {
+  return gulp.src(SYSTEM_JSON_FILES, {base: './'})
+    .pipe(gulp.dest('./dist'))
+}
+const copyJsonTask = gulp.series(copyJsonFiles);
+
+const SYSTEM_COPY = [
+  'src/packs/**/*',
+  'src/lang/**/*',
+  'src/module/**/*',
+  'src/templates/**/*',
+  '!src/assets/**/*.{png,jpeg,jpg}',
+  '!src/tokens/**/*.{png,jpeg,jpg}',
+];
+function copyFiles() {
+  return gulp.src(SYSTEM_COPY, {base: 'src'})
+    .pipe(gulp.dest('./dist'))
+}
+const copyTask = gulp.series(copyFiles);
+
+/* ----------------------------------------- */
+/* Watch Updates
 /* ----------------------------------------- */
 
 function watchUpdates() {
-  gulp.watch(SYSTEM_SCSS, css);
+  gulp.watch(SYSTEM_IMAGES, compileImages);
+  gulp.watch(SYSTEM_SCSS, cssTask);
+  gulp.watch(SYSTEM_COPY, copyTask);
+  gulp.watch(SYSTEM_JSON_FILES, copyJsonTask);
 }
 
 /* ----------------------------------------- */
-/*  Export Tasks
+/* Export Tasks
 /* ----------------------------------------- */
 
-exports.default = gulp.series(
+const defaultTask = gulp.series(
+  deleteFiles,
+  compileImages,
   compileScss,
+  copyFiles,
+  copyJsonFiles,
+  // copyManifest,
   watchUpdates
 );
-exports.build = gulp.series(
-  compileScss
+const buildTask = gulp.series(
+  deleteFiles,
+  compileImages,
+  compileScss,
+  copyFiles,
+  copyJsonFiles
 );
-exports.css = css;
+
+exports.default = defaultTask;
+exports.build = buildTask;
+
+exports.copy = copyTask;
+exports.images = imageTask;
+exports.css = cssTask;
+
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+patch = function() { return inc('patch') };
+major = function() { return inc('major') };
+minor = function() { return inc('minor') };
